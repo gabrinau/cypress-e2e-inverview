f25ea3e3c6a3a0341b20016d284a5df849cac27b
Felipe <martin.pacora@gmail.com>
2023-04-10 22:41:08 -0500

    adding before each

--- cypress/e2e/Login/validate_login.cy.js
+++ cypress/e2e/Login/validate_login.cy.js
@@ -1,4 +1,8 @@
 context("Validar Login", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
   it("Validar Login Satisfactorio", () => {});
 
   it("Validar Login NO Satisfactorio", () => {});
