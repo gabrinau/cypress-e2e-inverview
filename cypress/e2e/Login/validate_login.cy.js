470c6a5a113cd63c507d8bb0ff1cd5a50cbcd889
Leonardo Fernandez <leofernandez03@gmail.com>
2024-11-14 20:26:24 -0300

    Final commit

--- cypress/e2e/Login/validate_login.cy.js
+++ cypress/e2e/Login/validate_login.cy.js
@@ -1,68 +1,38 @@
-import pantallas from '../../selector/selectores'
+// La prueba conciste en generar un usuario con credenciales random creadas por la libreria faker, y con el mismo generar un flujo de compra exitoso
 
-const faker = require('faker');
-const usuario = faker.internet.userName(); 
-const contrasena = faker.internet.password(); 
-const nombre = faker.name.findName()
-const pais = faker.address.country();
-const ciudad = faker.address.city();
-const nroTarjeta = faker.finance.creditCardNumber('####-####-####-####'); 
-const mes = faker.date.future().getMonth() 
-const anio = faker.date.future().getFullYear(); 
 
 context('Generacion de usuario y compra exitosa', () => {
 
+
+  beforeEach(() => {
+
+    cy.visit('/') 
+
+  })
+  
+    
+it('Crear usuario', () =>{    
+    
+
+  cy.crearUsuario()
+
+  });
+
   it('Login exitoso', () => {
 
-    cy.visit('/');
+    cy.login()
 
-    cy.get(pantallas.crearUser.popupSingUp_btn).click().wait(3000)
+  });
 
-    cy.get(pantallas.crearUser.userNameSingUp_btn).type(usuario)
+  it('Agregar producto al carro de compras', () => {
 
-    cy.get(pantallas.crearUser.passSingUp_btn).type(contrasena)
+  cy.agregarProducto()
 
-    cy.get(pantallas.crearUser.singUp_btn).click()
-    
-
-    
-    // login
-
-    cy.get(pantallas.login.popupLogin_btn).click()
-
-    
-    cy.get(pantallas.login.userNameLogin_btn).type(usuario).wait(2000)
-    
-    cy.get(pantallas.login.passLogin_btn).type(contrasena).wait(2000)
-
-    cy.get(pantallas.login.singUp_btn).click().wait(3000)
-    
-    cy.get(pantallas.login.welcome_txt).should('include.text', `Welcome ${usuario}`)
-    
-    
-
-
-    //Agregar producto al carro de compras
-    cy.get(pantallas.product.selectProduct_btn).click().wait(2000)
-
-    cy.get(pantallas.product.addCart_btn).click()
+  });
   
-    cy.get(pantallas.product.placeOrder_btn).click()
+  it('Finalizar compra', () => {
 
-    // Finalizar compra
-    cy.get(pantallas.placeOrder.popUp_btn).click()
-    cy.get(pantallas.placeOrder.poName_txt).type(nombre)
-    cy.get(pantallas.placeOrder.poCountry_txt).type(pais)
-    cy.get(pantallas.placeOrder.poCity_txt).type(ciudad)
-    cy.get(pantallas.placeOrder.poCard_txt).type(nroTarjeta)
-    cy.get(pantallas.placeOrder.poMonth_txt).type(mes)
-    cy.get(pantallas.placeOrder.poYear_txt).type(anio)
-    cy.get(pantallas.placeOrder.poPurchase_btn).click()
-    cy.contains("h2", "Thank you for your purchase!").wait(2000)
+    cy.finCompra()
 
-    // confirmar compra 
-    cy.get(pantallas.placeOrder.poConfirm_btn).click()
-
-
-});
-});
+    })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,63 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
+import pantallas from '../selector/selectores'
+
+const faker = require('faker');
+const usuario = faker.internet.userName(); 
+const contrasena = faker.internet.password(); 
+const nombre = faker.name.findName()
+const pais = faker.address.country();
+const ciudad = faker.address.city();
+const nroTarjeta = faker.finance.creditCardNumber('####-####-####-####'); 
+const mes = faker.date.future().getMonth() 
+const anio = faker.date.future().getFullYear(); 
+
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+ Cypress.Commands.add('crearUsuario', ()=>{ 
+
+    cy.get(pantallas.crearUser.popupSingUp_btn).click().wait(3000)
+  
+    cy.get(pantallas.crearUser.userNameSingUp_btn).type(usuario)
+
+    cy.get(pantallas.crearUser.passSingUp_btn).type(contrasena)
+
+    cy.get(pantallas.crearUser.singUp_btn).click()
+
+})
+
+ Cypress.Commands.add('login', ()=>{ 
+
+    cy.get(pantallas.login.popupLogin_btn).click()
+
+    
+    cy.get(pantallas.login.userNameLogin_btn).type(usuario).wait(2000)
+    
+    cy.get(pantallas.login.passLogin_btn).type(contrasena).wait(2000)
+
+    cy.get(pantallas.login.singUp_btn).click().wait(5000)
+  
+    })
+
+
+ Cypress.Commands.add('agregarProducto', ()=>{ 
+    cy.get(pantallas.product.selectProduct_btn).click().wait(2000)
+
+    cy.get(pantallas.product.addCart_btn).click()
+
+
+ })
+
+ Cypress.Commands.add('finCompra', ()=>{ 
+
+    cy.get(pantallas.product.placeOrder_btn).click()
+    cy.get(pantallas.placeOrder.popUp_btn).click()
+    cy.get(pantallas.placeOrder.poName_txt).type(nombre)
+    cy.get(pantallas.placeOrder.poCountry_txt).type(pais)
+    cy.get(pantallas.placeOrder.poCity_txt).type(ciudad)
+    cy.get(pantallas.placeOrder.poCard_txt).type(nroTarjeta)
+    cy.get(pantallas.placeOrder.poMonth_txt).type(mes)
+    cy.get(pantallas.placeOrder.poYear_txt).type(anio)
+    cy.get(pantallas.placeOrder.poPurchase_btn).click()
+    cy.contains("h2", "Thank you for your purchase!").wait(2000)
+    cy.get(pantallas.placeOrder.poConfirm_btn).click()
+
+
+ })
