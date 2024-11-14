241e65b0255c7332fcccfded9762384e04f886bf
martinaemunoz <martinaemunoz@gmail.com>
2024-11-14 13:08:24 -0300

    initial commit

--- cypress/e2e/Login/validate_login.cy.js
+++ cypress/e2e/Login/validate_login.cy.js
@@ -1,5 +1,12 @@
 context("Validar Login", () => {
-  it("Validar Login Satisfactorio", () => {});
+  it("Validar Login Satisfactorio", () => {
+    cy.visit('https://www.demoblaze.com/index.html'); // se ingresa a la plataforma
 
-  it("Validar Login NO Satisfactorio", () => {});
+    cy.get('#login2').click(); // se ingresa al login
+    cy.get('#loginusername').clear().type('marti123', { delay: 100 }); // se ingresa usuario existente y contraseña (con delays para registrar cada letra)
+    cy.get('#loginpassword').clear().type('marti456', { delay: 100 });
+    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click(); // se clickea en el botón del modal Login
+    cy.get('#logInModal').should('not.be.visible'); // verificando que desaparezca el modal
+    cy.get('#nameofuser').should('contain', 'Welcome marti123'); // ingreso exitoso
+  });
 });
---
+++ cypress/e2e/Shopping Cart/validate_shopping_cart.cy.js
@@ -0,0 +1,6 @@
+context("Agregar productos", () => {
+    it("Agrega productos al carrito satisfactoriamente", () => {
+      cy.visit('https://www.demoblaze.com/index.html'); // se ingresa a la plataforma
+  
+    })
+  })
