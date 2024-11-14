afe877a8a6e3da2661799220f8b1e29ee0f34699
Leonardo Fernandez <leofernandez03@gmail.com>
2024-11-14 11:15:45 -0300

    Primer commit

--- cypress/e2e/Login/validate_login.cy.js
+++ cypress/e2e/Login/validate_login.cy.js
@@ -1,5 +1,16 @@
-context("Validar Login", () => {
-  it("Validar Login Satisfactorio", () => {});
+context("Validar Login ", () => {
+  it("Validar Login Satisfactorio", () => {
+    cy.visit('https://www.demoblaze.com/index.html')
+
+    //Crear una cuenta
+
+    //login
+    cy.get('a[data-target="#logInModal"]').click()
+    cy.wait(2000)
+    cy.get('#loginusername').type('lefedez')
+    cy.wait(2000)
+    cy.get('#loginpassword').type('rexmas')
+    cy.wait(2000)
 
   it("Validar Login NO Satisfactorio", () => {});
 });
